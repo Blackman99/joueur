016866d3f3b9d934d1122bd8abd2f808c7073bc1
Blackman99 <1197160272@qq.com>
2023-02-28 10:28:59 +0800

    assets: replace logos

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,4 +1,6 @@
-#![cfg_attr(
+use tauri::Manager;
+
+#[cfg_attr(
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
@@ -11,6 +13,15 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(debug_assertions)] // only include this code on debug builds
+            {
+                let window = app.get_window("main").unwrap();
+                window.open_devtools();
+                window.close_devtools();
+            }
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
