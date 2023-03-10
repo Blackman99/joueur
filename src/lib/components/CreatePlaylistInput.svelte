fdacaf7450cbf051bef4c03c505fed0c7d01ab97
Blackman99 <1197160272@qq.com>
2023-03-10 15:18:36 +0800

    feat: add playlist style optimize

--- src/lib/components/CreatePlaylistInput.svelte
+++ src/lib/components/CreatePlaylistInput.svelte
@@ -14,33 +14,45 @@
 </script>
 
 <div class="create-wrapper" class:focused="{focused}">
-  <div class="create-icon">
-    <CreatePlaylist />
-  </div>
   <input
     bind:value="{newPlaylistTitle}"
     on:keyup|stopPropagation="{handleKeyup}"
     on:focus="{() => (focused = true)}"
     on:blur="{() => (focused = false)}"
     class="create-input"
-    placeholder="Playlist title (Enter to add)"
   />
+  <div class="placeholder">
+    <div class="create-icon">
+      <CreatePlaylist />
+    </div>
+    <div>Playlist title (Enter to add)</div>
+  </div>
 </div>
 
 <style>
   .create-wrapper {
-    --uno: 'flex items-center bg-white pl-2 transition-colors transition-300';
+    --uno: 'flex h-[48px] items-center bg-white pl-2 relative';
+    transition: all linear 0.2s;
   }
   .focused {
-    --uno: 'bg-primary text-white';
+    --uno: 'bg-secondary text-white shadow-lg';
   }
-  .create-input::placeholder {
-    --uno: 'text-inherit';
+  .placeholder {
+    --uno: 'text-gray-4 absolute left-2 text-[14px] h-[16px] top-[16px] z-2 flex items-center';
+    transition: all linear 0.2s;
+    transform-origin: top left;
+  }
+  .focused .placeholder {
+    --uno: 'top-[2px] bottom-unset flex items-center';
+    transform: scale(0.8);
   }
   .create-input {
-    --uno: 'text-inherit pl-2 leading-10 flex-grow text-[14px] border-none outline-none bg-transparent';
+    --uno: 'relative flex-grow text-inherit text-[14px] w-full border-none outline-none bg-transparent z-3 my-0';
+  }
+  .focused .create-input {
+    --uno: 'top-1';
   }
   .create-icon {
-    --uno: 'text-5 flex items-center';
+    --uno: 'text-5 flex items-center text-gray-4';
   }
 </style>
