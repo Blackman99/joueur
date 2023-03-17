66624a535b5d562d85d4fd8d9bef68f064004a9e
Blackman99 <1197160272@qq.com>
2023-03-17 15:40:16 +0800

    feat: dropzone z-index should be larger in zen mode

--- src/lib/components/DropZone.svelte
+++ src/lib/components/DropZone.svelte
@@ -9,6 +9,6 @@
 
 <style>
   .drop-zone {
-    --uno: 'fixed top-4 left-4 bottom-4 right-4 b-1 b-white b-dashed rounded-lg bg-black bg-opacity-30 text-8 flex items-center justify-center text-white z-99';
+    --uno: 'fixed top-4 left-4 bottom-4 right-4 b-1 b-white b-dashed rounded-lg bg-black bg-opacity-30 text-8 flex items-center justify-center text-white z-999';
   }
 </style>
