77f7d2199375816b87d11d678884d6625993b17b
Dongsheng Zhao <1197160272@qq.com>
2023-03-25 13:20:09 +0800

    fix: drop loading mask should have a larger z index than title bar

--- src/lib/components/DropLoading.svelte
+++ src/lib/components/DropLoading.svelte
@@ -12,6 +12,6 @@
 
 <style>
   .drop-loading {
-    --uno: 'fixed top-4 left-4 bottom-4 right-4 b-1 b-white b-dashed rounded-lg bg-black bg-opacity-30 text-8 flex items-center justify-center text-white z-99';
+    --uno: 'fixed top-4 left-4 bottom-4 right-4 b-1 b-white b-dashed rounded-lg bg-black bg-opacity-30 text-8 flex items-center justify-center text-white z-199';
   }
 </style>
